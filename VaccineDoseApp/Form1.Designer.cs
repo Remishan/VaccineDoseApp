7715e603a128c38481bd3c26f7c061dfe2af1911
Remi shan <cherukattilremi@gmail.com>
2021-05-14 00:27:50 +0530

    Add project files.

---
+++ VaccineDoseApp/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace VaccineDoseApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtMedicine = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rbtnYes = new System.Windows.Forms.RadioButton();
+            this.rbtnNo = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.lblMedicneValidation = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(92, 142);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(117, 20);
+            this.txtAge.TabIndex = 0;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(92, 107);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(117, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtMedicine
+            // 
+            this.txtMedicine.Location = new System.Drawing.Point(92, 248);
+            this.txtMedicine.Name = "txtMedicine";
+            this.txtMedicine.Size = new System.Drawing.Size(117, 20);
+            this.txtMedicine.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Is Chain Smoker";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Medicne Dose";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(41, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(185, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Covid Vaccination";
+            // 
+            // rbtnYes
+            // 
+            this.rbtnYes.AutoSize = true;
+            this.rbtnYes.Location = new System.Drawing.Point(109, 212);
+            this.rbtnYes.Name = "rbtnYes";
+            this.rbtnYes.Size = new System.Drawing.Size(43, 17);
+            this.rbtnYes.TabIndex = 9;
+            this.rbtnYes.Text = "Yes";
+            this.rbtnYes.UseVisualStyleBackColor = true;
+            // 
+            // rbtnNo
+            // 
+            this.rbtnNo.AutoSize = true;
+            this.rbtnNo.Checked = true;
+            this.rbtnNo.Location = new System.Drawing.Point(170, 212);
+            this.rbtnNo.Name = "rbtnNo";
+            this.rbtnNo.Size = new System.Drawing.Size(39, 17);
+            this.rbtnNo.TabIndex = 10;
+            this.rbtnNo.TabStop = true;
+            this.rbtnNo.Text = "No";
+            this.rbtnNo.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(52, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(92, 177);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(117, 20);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.btnSave.Location = new System.Drawing.Point(92, 290);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(117, 34);
+            this.btnSave.TabIndex = 13;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // lblMedicneValidation
+            // 
+            this.lblMedicneValidation.AutoSize = true;
+            this.lblMedicneValidation.Location = new System.Drawing.Point(215, 251);
+            this.lblMedicneValidation.Name = "lblMedicneValidation";
+            this.lblMedicneValidation.Size = new System.Drawing.Size(0, 13);
+            this.lblMedicneValidation.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(267, 343);
+            this.Controls.Add(this.lblMedicneValidation);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.rbtnNo);
+            this.Controls.Add(this.rbtnYes);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMedicine);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtAge);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vaccination";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtMedicine;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rbtnYes;
+        private System.Windows.Forms.RadioButton rbtnNo;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label lblMedicneValidation;
+    }
+}
+
---
+++ VaccineDoseApp/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VaccineDoseApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "" && txtAge.Text == "" && txtEmail.Text == "" && txtMedicine.Text == "")
+            {
+                MessageBox.Show("Please fill all fields");
+            }
+            if (Convert.ToDouble(txtMedicine.Text) > 10)
+            {
+                lblMedicneValidation.Text = "Below 10";
+                lblMedicneValidation.ForeColor = Color.Red;
+            }
+            if (Convert.ToInt32(txtAge.Text)>=4&& Convert.ToInt32(txtAge.Text) <= 18)
+            {
+                if (rbtnNo.Checked==true&& Convert.ToDouble(txtMedicine.Text)>=1&& Convert.ToDouble(txtMedicine.Text)<=2.2)
+                {
+                    MessageBox.Show("Your Slot is Booked");
+                }
+                else if (Convert.ToDouble(txtMedicine.Text) >= 2.0 && Convert.ToDouble(txtMedicine.Text) <= 2.5&&rbtnYes.Checked == true)
+                {
+                    MessageBox.Show("Your Slot is Booked");
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter Medicine dosage between 1 to 2.5 ");
+                }
+            }
+            if (Convert.ToInt32(txtAge.Text) >= 19 && Convert.ToInt32(txtAge.Text) <= 30)
+            {
+                if (rbtnNo.Checked == true && Convert.ToDouble(txtMedicine.Text) >= 2.3 && Convert.ToDouble(txtMedicine.Text) <= 4.4)
+                {
+                    MessageBox.Show("Your Slot is Booked");
+                }
+                else if (Convert.ToDouble(txtMedicine.Text) >= 1.0 && Convert.ToDouble(txtMedicine.Text) < 2.0&& rbtnYes.Checked == true)
+                {
+                    MessageBox.Show("Your Slot is Booked");
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter Medicine dosage between 2.3 to 4.4 ");
+                }
+            }
+            if (Convert.ToInt32(txtAge.Text) >= 31 && Convert.ToInt32(txtAge.Text) <= 55)
+            {
+                if (rbtnNo.Checked == true && Convert.ToDouble(txtMedicine.Text) >= 4.4 && Convert.ToDouble(txtMedicine.Text) <= 6.0)
+                {
+                    MessageBox.Show("Your Slot is Booked");
+                }
+                else if (Convert.ToDouble(txtMedicine.Text) >= 1.0 && Convert.ToDouble(txtMedicine.Text) < 2.0 && rbtnYes.Checked == true)
+                {
+                    MessageBox.Show("Your Slot is Booked");
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter Medicine dosage between 4.4 to 6.0 ");
+                }
+            }
+            if ( Convert.ToInt32(txtAge.Text) >= 55)
+            {
+                if (rbtnNo.Checked == true && Convert.ToDouble(txtMedicine.Text) >= 3.3 && Convert.ToDouble(txtMedicine.Text) <= 4.5)
+                {
+                    MessageBox.Show("Your Slot is Booked");
+                }
+                else if (Convert.ToInt32(txtAge.Text) >= 55 && rbtnYes.Checked == true)
+                {
+                    MessageBox.Show("No Vaccine");
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter Medicine dosage between 3.3 to 4.5 ");
+                }
+            }
+            txtName.Clear();
+            txtAge.Clear();
+            txtEmail.Clear();
+            txtMedicine.Clear();
+        }
+    }
+}
